9fe58abe996d3650a5f5073849fa0f2c2fa4a5f2
MathiasRios2009 <mr8417240@gmail.com>
2026-04-20 10:29:58 -0500

    fin

---
+++ Actividad 2_Variables.py
@@ -0,0 +1,10 @@
+# %%
+first_name, last_name, country, age, is_maried="Mathias", "Rios", "Ecuador", 250, True
+
+print(first_name,last_name, country, age, is_maried)
+print("Nombre:",first_name)
+print("Apeellido",last_name)
+print("Pais: ", country)
+print("Edad: ", age)
+print("Casado: ", is_maried)
+# %%
