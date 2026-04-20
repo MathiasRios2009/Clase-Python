7b22fdc22072a104a99faa7f970ef2ea47386f70
MathiasRios2009 <mr8417240@gmail.com>
2026-04-20 10:31:26 -0500

    Evaluacion

---
+++ notas.py
@@ -0,0 +1,40 @@
+nombre="Mathias"
+apellido="Rios"
+nombre_completo=(nombre+" "+apellido)
+print(nombre_completo)
+
+print("Asi, Asi, Asi Gana el Madrid\n!Hala Madrid!")
+print("Champions\t+\tBarca=\tEliminado")
+print("This is a backlash symbol(\\)")
+
+
+print("nombre: ", nombre, "apellido: ", apellido)
+print(f"nombre: {nombre} apellido: {apellido}")
+#%%
+nombre='Mathias'
+j,a,h,i,m,t,s=nombre
+print(j)
+print(a)
+print(h)
+print(i)
+print(m)
+print(t)
+print(s)
+#%%'
+
+first_letter = nombre[0]
+
+print(first_letter) # P
+
+second_letter = nombre[1]
+
+print(second_letter) # y
+
+last_index = len(nombre) - 1
+
+last_letter = nombre[last_index]
+
+print(last_letter) # n
+
+
+# %%
