713c0ae04d002a63e37e1562e9ccf529cbe11aef
MathiasRios2009 <mr8417240@gmail.com>
2026-05-20 12:30:17 -0500

    Update Actividad6.py

--- Actividad6.py
+++ Actividad6.py
@@ -5,3 +5,10 @@ num=int(input('ingrese un numero: '))
 for i in range(desde,hasta+1):
     por=num*i
     print(f'{num}x{i}={por}')
+
+notas=[5, 9, 4, 5, 10]
+suma=0
+for i in range(1,4):
+    suma=suma+notas[i]
+promedio=suma/3
+print(f"El promedio es: {promedio}")
