36ba20813a2a9149c7b870cd873e263efbf04ff4
MathiasRios2009 <mr8417240@gmail.com>
2026-06-05 08:21:42 -0500

    Create Actividad 7.py

---
+++ Actividad 7.py
@@ -0,0 +1,15 @@
+def obtener_mensaje():
+    mensaje = "Bienvenido al sistema"
+    return mensaje
+
+
+def generar_nombre_completo():
+    nombre = "Mathias"
+    apellido = "Rios"
+    espacio = " "
+    nombre_completo = nombre + espacio + apellido
+    return nombre_completo
+
+
+print(obtener_mensaje())
+print(generar_nombre_completo())
